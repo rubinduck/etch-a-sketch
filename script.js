59513321a9a341347812fa128c6885358f83ef89
rubinduck <rubinduck@protonmail.com>
2022-02-06 21:59:03 +0100

    Move grid functions to class

--- script.js
+++ script.js
@@ -46,44 +46,64 @@ const randomInt = (min, max) =>
 
 
 
-const defaultCellColor = RGB.White;
-function clearGrid(gridElement){
-    const cells = gridElement.querySelectorAll('.cell');
-    cells.forEach(cell => cell.style.backgroundColor = defaultCellColor.toString());
+class Grid {
+    defaultColor = RGB.White;
+    drawingColor = RGB.Black;
+    gridElement;
+    cellElements = [];
+    sideSizePx;
+    constructor(gridElement, cellsPerSide){
+        this.gridElement = gridElement;
+        this.sideSizePx = gridElement.clientHeight;
+        this.#spawnGridCells(cellsPerSide);
+    }
+
+    clear(){
+        this.cellElements.forEach(cell => setBackgroundColor(cell, this.defaultColor));
+    }
+
+    resize(cellsPerSize){
+        this.#spawnGridCells(cellsPerSize);
+    }
+
+    #spawnGridCells(cellsPerSide){
+        const cellAmount = cellsPerSide * cellsPerSide;
+        const cellSize = this.sideSizePx / cellsPerSide;
+        this.cellElements = this.#createCells(cellSize, cellAmount);
+        this.gridElement.replaceChildren(...this.cellElements);
+    }
+
+    #createCells(cellSizePx, amount){
+        const cells = [];
+        for (let i = 0; i < amount; i++)
+            cells.push(this.#createCell(cellSizePx));
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', this.handleMouseOverCell);
+            setBackgroundColor(cell, this.defaultColor)
+        });
+        return cells;
+    }
+
+    #createCell(sizePx){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.height = `${sizePx}px`;
+        cell.style.width = `${sizePx}px`;
+        return cell;
+    }
+
+    handleMouseOverCell(event){
+        console.log('here')
+        const element = event.currentTarget;
+        const currentColor = getBackgroundColor(element);
+        const newColor = RGB.average(currentColor, RGB.Black);
+        setBackgroundColor(element, newColor)
+    }
 }
 
-function genGridCells(gridElement, cellsPerSide){
-    const cellAmount = cellsPerSide * cellsPerSide;
-    const gridSideSize = gridElement.clientHeight;
-    const cellSize = gridSideSize / cellsPerSide;
-    const newCells = createCells(cellSize, cellAmount);
-    gridElement.replaceChildren(...newCells);
-}
 
-function createCells(cellSizePx, amount){
-    const cells = [];
-    for (let i = 0; i < amount; i++)
-        cells.push(createCell(cellSizePx));
-    cells.forEach(cell => {
-        cell.addEventListener('mouseover', handleMouseOverCell);
-        cell.style.backgroundColor = defaultCellColor.toString();
-    });
-    return cells;
-}
-
-function handleMouseOverCell(event){
-    const element = event.currentTarget;
-    const currentColor = getBackgroundColor(element);
-    const newColor = RGB.average(currentColor, RGB.Black);
-    element.style.backgroundColor = newColor.toString();
-}
-
-function createCell(sizePx){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.style.height = `${sizePx}px`;
-    cell.style.width = `${sizePx}px`;
-    return cell;
+function setBackgroundColor(element, rgbColor){
+    element.style.backgroundColor = rgbColor.toString();
 }
 
 function getBackgroundColor(element){
@@ -97,40 +117,39 @@ function fitlerNotInts(event){
     const target = event.currentTarget;
     if (!isInt(newData))
         target.value = target.value.replace(newData, '');
-}
 
-function isInt(string){
-    for (let char of string){
-        if (char < '0' || char > '9')
-            return false;
+    function isInt(string){
+        for (let char of string){
+            if (char < '0' || char > '9')
+                return false;
+        }
+        return true;
     }
-    return true;
 }
 
-function handleResizeButton(gridElement, gridSizeElement){
+function handleResizeButton(grid, gridSizeElement){
     const newSideSize = Number.parseInt(gridSizeElement.value);
     if (newSideSize < 2 || newSideSize > 100){
         alert('Side must be between 2 and 100');
         return;
     }
-    genGridCells(gridElement, newSideSize);
+    grid.resize(newSideSize)
 }
 
 function main(){
     const gridElement = document.getElementById('cells-grid');
     const startCellsPerSideAmount = 16;
+    const grid = new Grid(gridElement, startCellsPerSideAmount);
 
     const clearButton = document.getElementById('clear-button');
-    clearButton.addEventListener('click',(e) =>
-        clearGrid(gridElement))
+    clearButton.addEventListener('click',(e) => grid.clear())
 
     const gridSideSizeElement = document.getElementById('side-size-input');
     gridSideSizeElement.addEventListener('input', fitlerNotInts)
     const resizeButton = document.getElementById('resize-button');
-    resizeButton.addEventListener('click', (e) =>
-        handleResizeButton(gridElement, gridSideSizeElement));
+    resizeButton.addEventListener('click', (e) => handleResizeButton(grid, gridSideSizeElement));
 
-    genGridCells(gridElement, startCellsPerSideAmount);
+    //  genGridCells(gridElement, startCellsPerSideAmount);
 }
 
 main();
